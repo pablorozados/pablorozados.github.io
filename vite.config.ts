951fc1f7ce47422c3696355300465ad1dba4aa96
pabloprime <pablorozados@gmail.com>
2025-08-13 08:57:06 -0300

    admin no vite

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  routes: [
+    {
+      path: '/admin',
+      component: 'src/admin/index.html',
+    },
+  ],
 }));
